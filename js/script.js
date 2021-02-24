81a74ea0d23b9cff544cec46b02b949be36d4eff
lukagoderdzishvili <67374477+lukagoderdzishvili@users.noreply.github.com>
2021-02-24 21:09:45 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".header").addClass("active");
+    } else {
+       $(".header").removeClass("active");
+    }
+});
+
